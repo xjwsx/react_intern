c9279855c6102bb55b0125391e6e58e3c36a8586
xjwsx <wlstmdghs@naver.com>
2024-08-09 18:10:48 +0900

    feat: zustand 구조

---
+++ src/store/useUserStore.tsx
@@ -0,0 +1,21 @@
+import create from "zustand";
+
+interface UserState {
+  zuserId: string | null;
+  znickname: string | null;
+  isSaved: boolean;
+  setUser: (userId: string, nickname: string) => void;
+  clearUser: () => void;
+}
+
+const useUserStore = create<UserState>((set) => ({
+  zuserId: null,
+  znickname: null,
+  isSaved: false,
+
+  setUser: (userId, nickname) =>
+    set({ zuserId: userId, znickname: nickname, isSaved: true }),
+  clearUser: () => set({ zuserId: null, znickname: null, isSaved: false }),
+}));
+
+export default useUserStore;
