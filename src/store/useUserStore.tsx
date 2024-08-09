c3de3bca66f76c9e3ceb76991091d4afc089c3e9
xjwsx <wlstmdghs@naver.com>
2024-08-09 22:36:20 +0900

    feat: zustand 상태관리 추가

--- src/store/useUserStore.tsx
+++ src/store/useUserStore.tsx
@@ -1,4 +1,4 @@
-import create from "zustand";
+import { create } from "zustand";
 
 interface UserState {
   zuserId: string | null;
@@ -14,7 +14,11 @@ const useUserStore = create<UserState>((set) => ({
   isSaved: false,
 
   setUser: (userId, nickname) =>
-    set({ zuserId: userId, znickname: nickname, isSaved: true }),
+    set({
+      zuserId: userId,
+      znickname: nickname,
+      isSaved: true,
+    }),
   clearUser: () => set({ zuserId: null, znickname: null, isSaved: false }),
 }));
 
